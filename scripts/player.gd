7ae14f24ac6bb19886281e6fcf793025ecf5efaa
Illia Kovalchuk <136240492+JustSadSock@users.noreply.github.com>
2025-11-26 15:32:01 +0200

    Fix gravity retrieval in player script

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-const GRAVITY = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 const DEFAULT_PROJECTILE_SCENE := preload("res://scenes/Projectile.tscn")
 const DEFAULT_EXPLOSION_SCENE := preload("res://scenes/Explosion.tscn")
@@ -46,7 +46,7 @@ func _unhandled_input(event):
 
 func _physics_process(delta):
     if not is_on_floor():
-        velocity.y -= GRAVITY * delta
+        velocity.y -= gravity * delta
     var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
     var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
     var target_speed = speed * (sprint_multiplier if Input.is_action_pressed("sprint") else 1.0)
